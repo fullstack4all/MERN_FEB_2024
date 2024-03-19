e02b3dddd0027a7cd32e560033024b1df4b8abf0
fullstack4all <160142156+fullstack4all@users.noreply.github.com>
2024-03-19 18:59:04 +0530

    User Routes and Controller

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 
 const place = require('./router/place')
+
+const user = require('./router/user-routes')
+
 const bodyParser = require('body-parser')
 
 
 const app = express()
+
+
     app.use(bodyParser.json())
+
+    app.use('/api/places', place)
+    app.use('/api/users', user)
     app.use(place)
 
-app.listen(5000)
+app.listen(5000);
---
+++ userController.js
@@ -0,0 +1,42 @@
+const Dummy_User = [
+    {
+        id: 'u1',
+        name:'sam',
+        email:'test@test.com',
+        password:'password'
+    }
+];
+
+const getUsers = (req, res, next)=>{
+    res.json({users:Dummy_User})
+
+};
+const signup = (req, res, next)=>{
+    const {name, email, password} = req.body;
+
+    const createdUser = {
+        name,
+        email,
+        password
+
+    }
+    Dummy_User.push(createdUser)
+    res.status(201).json({user: createdUser})
+};
+const login = (req, res, next)=>{
+
+    const{email, password} = req.body;
+
+    const identifiedUser = Dummy_User.find(u => u.email === email)
+
+        if(!identifiedUser || identifiedUser.password != password){
+            res.status(404).json({message:'Invalid email or password !'})
+        }
+
+        res.json({message:'Logged in!'})
+
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login
