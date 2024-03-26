aa9d07f2ea7634da4b724b1ce8f5a3ad6a1d5a2c
fullstack4all <160142156+fullstack4all@users.noreply.github.com>
2024-03-26 18:55:07 +0530

    Connecting Node with MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const mongoose = require('mongoose')
+
 const place = require('./router/place')
 
 const user = require('./router/user-routes')
@@ -10,10 +12,18 @@ const bodyParser = require('body-parser')
 const app = express()
 
 
+
     app.use(bodyParser.json())
+
+  
 
     app.use('/api/places', place)
     app.use('/api/users', user)
     app.use(place)
 
-app.listen(5000);
+    mongoose.connect("mongodb+srv://fullstack4all:@cluster0.jtswj0p.mongodb.net/Database?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
+        app.listen(5000);
+    }).catch(err =>{
+        console.log(err);
+    })
+
